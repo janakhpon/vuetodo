efcc66a2fc72b0d1637bed89d6ce6998b55cb253
janakhpon <jnovaxer@gmail.com>
2019-07-05 13:21:32 +0630

    GDEV004

--- src/views/tasks/UpdateTask.vue
+++ src/views/tasks/UpdateTask.vue
@@ -14,38 +14,10 @@
 </template>
 
 <script>
-    import * as taskService from '../../services/TaskService'
+
 
     export default {
         name: 'update-task',
-        data: function() {
-            return {
-                task: {
-                    text:''
-                }
-            }
-        },
-        beforeRouteEnter(to, from, next) {
-            taskService.getTask(to.params.id)
-                .then(response => {
-                    if (!response) {
-                        next('/list');
-                    } else {
-                        next(vm => {
-                            const task = response.data.task;
-                            vm.task = task;
-                        })
-                    }
-                });
-        },
-        methods: {
-            onSubmit: async function() {
-                const request = {
-                    task: this.task
-                }
-                await taskService.updateTask(request);
-                this.$router.push({ name: 'list' });
-            }
-        }
+        
     }
 </script>
